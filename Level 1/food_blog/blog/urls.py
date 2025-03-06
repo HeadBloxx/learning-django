a25a7352e94bbea6b2c9aaa2b580045670005c75
HeadBloxx <bloxx@gmail.com>
2025-03-06 18:49:53 +0100

    upd

--- Level 1/food_blog/blog/urls.py
+++ Level 1/food_blog/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("posts/<int:post_id>", views.posts, name="posts")
 ]
--- Level 1/food_blog/blog/views.py
+++ Level 1/food_blog/blog/views.py
@@ -4,5 +4,31 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts_dict = {
+
+    "1": {
+        "title": "title1",
+        "image": "Copper",
+        "content": "content1",
+        "author": "John Doe",
+        "publish_date": "01.01.1999.",
+    },
+    "2": {
+        "title": "title2",
+        "image": "Uranium",
+        "content": "content2",
+        "author": "Jane Doe",
+        "publish_date": "05.12.2049."
+    }
+
+}
+
 def home(request):
-    return render(request, "blog/home.html")
+    print("called", posts_dict)
+    return render(request, "blog/home.html", {"posts": posts_dict})
+
+def about(request):
+    return render(request, "blog/about.html")
+
+def posts(request, post_id):
+    return render(request, "blog/posts.html", {"post": posts_dict[str(post_id)]})
